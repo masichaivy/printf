eef536ebdfa8f1ccd4efc6ebe5853a1e3dcb8dab
masichaivy <lyndamasicha@gmail.com>
2023-07-25 09:52:26 -0700

    initialize _printf file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int char_count;
+
+	char_count = 0;
+}
